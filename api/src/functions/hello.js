0d8f4370a6fb09c76dcc5c3066ec9a2f4e851f15
Rob Cameron <rob.cameron@fastmail.com>
2020-07-31 11:14:34 -0700

    Adds test function

---
+++ api/src/functions/hello.js
@@ -0,0 +1,10 @@
+import { db } from 'api/src/lib/db'
+
+export const handler = async (event, context) => {
+  const users = await db.user.findMany()
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(users),
+  }
+}
